42509c532effb7a0e91bccb15fb3899f38f0d0ff
Glitchy Scripts <glitchyscripts@hotmail.com>
2023-11-12 16:15:43 +1000

    Added an advanced filter site by tag system and added adult.html which includes sites with adult tag.

--- build.js
+++ build.js
@@ -2,57 +2,331 @@ import fs from "fs"
 import yaml from "yaml"
 import {marked} from "marked"
 
-
-let directory = yaml.parse(fs.readFileSync('./directory.yml', 'utf8'));
-
-let sites = {};
-let siteList = yaml.parse(fs.readFileSync('./sites.yml', 'utf8'));
-let sitesNotListed = new Set();
-let sitesNotCompiled = new Set();
-
-let adultContent = fs.existsSync("./.ADULT")
-
-for(let site of siteList)
+function BuildSiteElement(site_data)
 {
-	if(site.name == "" || site.name == null)
-		continue;
-
-	if(sites[site.name])
-		throw new Error("Site " + site.name + " was already added.")
-
-	sites[site.name] = site;
-	sitesNotListed.add(site.name)
+	const protocol = "//";
+	//XXX: Use this when testing locally (without a web server).
+	//const protocol = "https://";
+	
+	const GetUrl = (link) => {
+		if (link.indexOf("://") >= 0) {
+			return link;
+		}
+		return protocol + link;
+	};
+	
+	const GetTagLabelGroupingClass = (idx_tag, tags) => {
+		if (tags.length > idx_tag + 1 && tags[idx_tag + 1][0].startsWith(tags[idx_tag][0] + ":")) {
+			return "tag-label-start";
+		}
+		else if (tags[idx_tag][0].indexOf(":") >= 0) {
+			if (tags.length <= idx_tag + 1 || tags[idx_tag + 1][0].indexOf(":") < 0) {
+				return "tag-label-end";
+			}
+			else {
+				return "tag-label-middle";
+			}
+		}
+		else {
+			return "";
+		}
+	};
+	
+	return `\
+<div class="site-container ${site_data.tags.has("dead") ? "dead" : ""}" data-tags="${Array.from(site_data.tags.keys()).join(",")}" >
+	<div>
+		<div class="site-title" >${site_data.name}</div>
+		<span class="site-updated" >Checked on ${site_data.updated}</span>
+	</div>
+	<div class="site-body-container" >
+		<div class="site-link-container" >
+` + (site_data.link == undefined
+? ""
+: `			<a class="site-link" href="${GetUrl(site_data.link)}" >${site_data.link}</a>
+`) + `\
+` + (site_data.description == undefined
+? ""
+: `			<div class="site-description" >${site_data.description}</div>
+`) + `\
+		</div>
+` + Array.from(site_data.links.values()).map((link_data) => `\
+		<div class="site-link-container ${link_data.is_dead ? "dead" : ""}" >
+			<a class="site-link" href="${GetUrl(link_data.link)}" >${link_data.link}</a>
+` + (link_data.description == undefined
+? ""
+: `			<div class="site-description" >${link_data.description}</div>
+`) + `\
+		</div>
+`).join("") + `\
+		<div>
+` + Array.from(site_data.tags).map((tag_data, idx_tag, tags) => `\
+<div class="tag-label ${GetTagLabelGroupingClass(idx_tag, tags)}" name="${tag_data[0]}" >${tag_data[1].name}</div>\
+`).join("") + `\
+		</div>
+	</div>
+</div>
+`;
 }
 
-let struct = buildStructure(directory.body);
-
-// Parse TOC
-let siteText = flatten(stripSitelessNodes(buildStructure(directory.body)));
-
-let nav = toTOC(stripSitelessNodes(buildStructure(directory.body)));
-
-if(adultContent)
-	siteText = siteText.replace("{{intro}}", directory.intro_ad)	
-else
-	siteText = siteText.replace("{{intro}}", directory.intro)	
-
-
-let siteMatch = /{{(.*?)}}/.exec(siteText)
-
-while(siteMatch)
+function BuildTagElement(tag_data)
 {
-	siteText = siteText.replace(siteMatch[0], buildSite(siteMatch[1]))
-	siteMatch = /{{(.*?)}}/.exec(siteText)
+	return `\
+<div class="tag-container" >
+	<label><input type="checkbox" name="${tag_data.tag}" onclick="CheckboxToggleIndeterminate(this)" onchange="ToggleTag(this)" autocomplete="off" indeterminate /> ${tag_data.name}</label>
+` + (tag_data.description == undefined
+? ""
+: `	<div class="tag-description" >${tag_data.description}</div>
+`) + (
+tag_data.subtags.size <= 0 ? "" : `\
+	<div class="tag-subtag-container" >
+` + Array.from(tag_data.subtags.values()).map((subtag_data) => `\
+		<label><input type="checkbox" name="${subtag_data.id}" onclick="CheckboxToggleIndeterminate(this)" onchange="ToggleTag(this)" autocomplete="off" indeterminate /> ${subtag_data.name}</label>
+`).join("") + `\
+	</div>
+`) + `\
+</div>
+`;
 }
 
-if(sitesNotListed.size > 0)
+function ParseTags()
 {
-	console.log([...sitesNotListed])
-	throw new Error("Not all registered sites are on the list")
+	let fileTextTags = fs.readFileSync('./tags.yml', 'utf8');
+	let dataTags = yaml.parse(fileTextTags);
+	
+	const tags = new Map();
+	
+	let tagOrderId = 0;
+	
+	for (const tag of dataTags) {
+		if (tag.tag == undefined) {
+			continue;
+		}
+		let tagData = {
+			tag: tag.tag,
+			name: tag.name,
+			id: tag.tag,
+			order_id: ++tagOrderId,
+			description: tag.description,
+			subtags: new Map(),
+		};
+		if (tagData.name == undefined) {
+			throw new Error(`Tag ${tagData.tag} does not have a name.`);
+		}
+		if (tag.subtags != undefined) {
+			for (const subtag of tag.subtags) {
+				if (subtag.tag == undefined) {
+					continue;
+				}
+				let subtagData = {
+					tag: subtag.tag,
+					name: subtag.name,
+					id: `${tagData.tag}:${subtag.tag}`,
+					order_id: ++tagOrderId,
+				};
+				if (subtagData.name == undefined) {
+					throw new Error(`Tag ${subtag.id} does not have a name.`);
+				}
+				if (tagData.subtags.has(tagData.tag)) {
+					throw new Error(`Tag ${subtag.id} already exists.`);
+				}
+				tagData.subtags.set(subtag.tag, subtagData);
+			}
+		}
+		
+		if (tags.has(tagData.tag)) {
+			throw new Error(`Tag ${tagData.tag} already exists.`);
+		}
+		tags.set(tagData.tag, tagData);
+	}
+	
+	return tags;
 }
 
+function ParseSites(tags)
+{
+	let fileTextSites = fs.readFileSync('./sites.yml', 'utf8');
+	let dataSites = yaml.parse(fileTextSites);
+	
+	const sites = new Map();
+	
+	const all_links = new Set();
+	const CheckLink = (link) => {
+		if (link == undefined) {
+			return false;
+		}
+		
+		if (all_links.has(link)) {
+			console.error(`WARNING: The link "${link}" has already been used in another site entry.`);
+			return false;
+		}
+		
+		all_links.add(link);
+		return true;
+	}
+	
+	for (const site of dataSites) {
+		if (site.name == undefined) {
+			continue;
+		}
+		let siteData = {
+			name: site.name,
+			description: site.description,
+			tags: new Map(),
+			link: site.link,
+			links: new Map(),
+			updated: site.updated,
+		};
+		
+		CheckLink(siteData.link);
+		
+		if (site.links !== undefined) {
+			for (const link of site.links) {
+				if (link.link == undefined) {
+					continue;
+				}
+				let linkData = {
+					link: link.link,
+					description: link.description,
+					is_dead: link.is_dead ?? false,
+				};
+				
+				if (siteData.links.has(linkData.link) || siteData.link == linkData.link) {
+					throw new Error(`Site "${siteData.name}" already contains the link: "${linkData.link}".`);
+				}
+				CheckLink(linkData.link);
+				siteData.links.set(linkData.link, linkData);
+			}
+		}
+		
+		const unparsedTags = site.tags?.split(",").map((tag) => tag.split(":").filter((tag) => tag.length > 0)).filter((tag) => tag.length > 0) ?? [];
+		for (const siteTag of unparsedTags) {
+			const tag = tags.get(siteTag[0]);
+			if (tag == undefined) {
+				throw new Error(`Site "${siteData.name}" tag "${siteTag[0]}" does not exist.`);
+			}
+			siteData.tags.set(tag.tag, tag);
+			
+			for (const siteSubtag of siteTag.slice(1)) {
+				const subtag = tag.subtags.get(siteSubtag);
+				if (subtag == undefined) {
+					throw new Error(`Site "${siteData.name}" tag "${tag.tag}:${siteSubtag}" does not exist.`);
+				}
+				
+				siteData.tags.set(subtag.id, subtag);
+			}
+		}
+		
+		if (siteData.tags.size <= 0) {
+			throw new Error(`Site "${siteData.name}" does not have any tags.`);
+		}
+		
+		if (siteData.link == undefined && siteData.links.size <= 0) {
+			throw new Error(`Site "${siteData.name}" does not have any links.`);
+		}
+		
+		if (siteData.updated == undefined) {
+			throw new Error(`Site "${siteData.name}" last updated date not set.`);
+		}
+		
+		if (sites.has(siteData.name)) {
+			throw new Error(`Site "${siteData.name}" already exists.`);
+		}
+		sites.set(siteData.name, siteData);
+	}
+	
+	return sites;
+}
 
-let html = `
+(function () {
+	const tags = ParseTags();
+	const sites = ParseSites(tags);
+	
+	let fileTextPage = fs.readFileSync('./directory.yml', 'utf8');
+	let dataPage = yaml.parse(fileTextPage);
+	
+	for (const adultContent of [false, true]) {
+		let navHeadings = [];
+		const AddNavHeading = (tag_data) => {
+			let leftPadding = 5;
+			if (tag_data.id.indexOf(":") >= 0) {
+				leftPadding += 25;
+			}
+			navHeadings.push(`\
+<a style="padding-left: ${leftPadding}px" href="#${tag_data.id}">${tag_data.name}</a>
+`);
+		};
+		
+		let lastTagId = "";
+		const GetSortingTag = (site_data) => {
+			const itrTag = site_data.tags.values();
+			const tag1 = itrTag.next().value;
+			const tag2 = itrTag.next().value;
+			
+			if ((tag2?.id.indexOf(":") ?? -1) >= 0) {
+				return tag2;
+			}
+			return tag1;
+		};
+		let htmlSites = Array
+			.from(sites.values())
+			.filter((site_data) => adultContent || !site_data.tags.has("adult"))
+			.sort((site_1, site_2) => GetSortingTag(site_1).order_id - GetSortingTag(site_2).order_id)
+			.map((site_data) => {
+				const siteHtml = BuildSiteElement(site_data);
+				const siteMainTagId = GetSortingTag(site_data).id;
+				let result = siteHtml;
+				if (lastTagId !== siteMainTagId) {
+					const tagParts = siteMainTagId.split(":");
+					const lastTagParts = lastTagId.split(":");
+					const BuildMajorHeading = (tag_id) => {
+						const tagData = tags.get(tag_id);
+						AddNavHeading(tagData);
+						return `\
+<div id="${tagData.id}" >
+	<h1>${tagData.name}</h1>
+` + (tagData.description == undefined
+? ""
+: `	<p>${tagData.description}</p>
+`) + `\
+</div>
+`;
+					};
+					const BuildMinorHeading = (tag_id) => {
+						const tagParts = tag_id.split(":");
+						const tagData = tags.get(tagParts[0]);
+						const subtagData = tagData.subtags.get(tagParts[1]);
+						AddNavHeading(subtagData);
+						return `\
+<div id="${subtagData.id}" >
+	<h2>${subtagData.name}</h2>
+</div>
+`;
+					};
+					let majorHeading = "";
+					let minorHeading = "";
+					if (tagParts.length > 1 && lastTagParts[0] !== tagParts[0]) {
+						majorHeading = BuildMajorHeading(tagParts[0]);
+					}
+					if (tagParts.length > 1) {
+						minorHeading = BuildMinorHeading(siteMainTagId);
+					}
+					else {
+						majorHeading = BuildMajorHeading(siteMainTagId);
+					}
+					lastTagId = siteMainTagId;
+					result = majorHeading + minorHeading + result;
+				}
+				return result;
+			})
+			.join("");
+		
+		let htmlTags = Array
+			.from(tags.values())
+			.filter((tag_data) => adultContent || tag_data.tag !== "adult")
+			.map((tag_data) => BuildTagElement(tag_data))
+			.join("");
+		
+		let html = `\
 <!DOCTYPE html>
 <html>
 	<head>
@@ -62,191 +336,29 @@ let html = `
 		<div id='background' class="${adultContent ? "adult" : ""}"></div>
 		<nav>
 			<strong>Table of Contents</strong>
-			${nav}
+			${navHeadings.join("")}
 		</nav>
 		<main>
-			${marked.parse(siteText)}
+			${marked.parse(adultContent ? dataPage.intro_ad : dataPage.intro)}
+			<label><input type="checkbox" id="SimpleFilterDead" onchange="ToggleDeadWebsites(this)" autocomplete="off" checked /> Show dead websites</label>
+			<div>
+				<h1>Advanced Filters</h1>
+				<input type="button" class="tags-reset-button" onclick="ResetAllTags(this)" value="Reset Tag Filters" />
+				<input type="button" class="tags-visibility-button" onclick="ToggleTagFiltersVisibility(this)" value="Show/Hide Filters" />
+				<div id="TagsContainer" class="tag-filters tag-filters-hide" >
+					${htmlTags}
+				</div>
+			</div>
+			<h1>Fan Works</h1>
+			<div id="SitesContainer" >
+				${htmlSites}
+			</div>
 		</main>
 		<script src="index.js"></script>
 	</body>
 </html>
 `;
-
-
-fs.writeFileSync('./index.html', html, 'utf8');
-
-
-function buildSite(siteName)
-{
-	let siteData = sites[siteName];
-
-	if(!siteData)
-	{
-		throw new Error("Site " + siteName + " is not included in sites.yml")
+		
+		fs.writeFileSync(adultContent ? "./adult.html" : "./index.html", html, "utf8");
 	}
-
-	if((siteData.is_adult && !adultContent) || (!siteData.is_adult && adultContent))
-		return "";
-
-	
-	let classes = "";
-
-	if(siteData.is_dead)
-		classes += " dead";
-	if(siteData.is_adult)
-		classes += " adult";
-
-	let protocol = "https://"
-	if(siteData.no_https)
-		protocol = "http://"
-
-	return `<div class="${classes}">
-	    <a class='site' href="${protocol + siteName}">${siteName}</a>
-	    <span>${siteData.description}</span>
-	    <span class='updated'>Checked on ${siteData.updated}</span>
-	  </div>`
-}
-
-function buildStructure(text)
-{
-	let lines = text.split("\n")
-
-	let currentStructure = {level:0, children: [], text:""};
-
-	for(let line of lines)
-	{
-		let s = line.trim()
-		if(s.startsWith("#"))
-		{	
-			let level=1
-			while(s[level] == "#")
-				level++;
-
-			let newStructure = {
-				type: "heading",
-				level,
-				text: line,
-				children: []
-			}
-
-			while(currentStructure.level >= level)
-				currentStructure = currentStructure.parent;
-
-			newStructure.parent = currentStructure;
-			currentStructure.children.push(newStructure);
-			currentStructure = newStructure;
-		}
-		else if (s.startsWith("{"))
-		{
-			let siteName = s.replace(/\{|\}/g,"").trim();
-
-			if(!sites[siteName])
-			{
-				currentStructure.children.push({type: "text", text: line})
-				continue;
-			}
-
-			sitesNotListed.delete(siteName)
-
-			if((adultContent && sites[siteName].is_adult) || (!adultContent && !sites[siteName].is_adult))
-			{
-				currentStructure.children.push({type: "site", text: line})
-			}
-		}
-		else
-		{
-			currentStructure.children.push({type: "text", text: line})
-		}
-	}
-
-	while(currentStructure.level != 0)
-		currentStructure = currentStructure.parent;
-
-	return currentStructure;
-}
-
-function stripSitelessNodes(node)
-{
-	if(node.type == "text")
-		return node;
-	if(node.type == "site")
-		return node;
-
-	for(let i=0; i < node.children.length; i++)
-	{
-		if(node.children[i].type == "text")
-			continue;
-		if(node.children[i].type == "site")
-			continue;
-
-		stripSitelessNodes(node.children[i]);
-
-		if(!hasSites(node.children[i]))
-		{
-			node.children.splice(i,1);
-			i--
-		}
-	}
-
-	return node;
-}
-
-function flatten(node)
-{
-	if(node.type == "text" || node.type == "site")
-		return node.text + "\n";
-
-	let headingHTML = "";
-	if(node.level > 0)
-	{
-		let text = node.text.replace(/#/g,"").trim();
-		let tag = "h"+node.level
-		headingHTML= `<${tag} id="${getHeadingAnchor(node.text)}">${text}</${tag}>`;
-	}
-
-	return headingHTML + node.children.map(x => flatten(x)).join("");
-}
-
-function getHeadingAnchor(text)
-{
-	text = text.replace(/#/g,"").trim();
-	return text.replace(/[^A-Za-z0-9 ]/g,"").replace(/ +/g,"_")
-}
-
-function toTOC(node)
-{
-	if(node.type == "text" || node.type == "site")
-		return "";
-
-	let section = node.text.replace(/#/g,"").trim();
-
-	let sectionLink = getHeadingAnchor(node.text);
-
-	let thisNode = node.level == 0 ? "" : `<a style="padding-left: ${node.level*25-20}px" href="#${sectionLink}">${section}</a>\n`
-
-
-	return thisNode+ node.children.map(toTOC).join("")
-
-}
-
-function hasSites(node)
-{
-	if(node.type == "text")
-	{
-		return false;
-	}
-	if(node.type == "site")
-	{
-		return true;
-	}
-
-	for(let k of node.children)
-	{
-		if(hasSites(k))
-		{
-			return true;
-		}
-	}
-
-	return false;
-}
+})();
--- index.js
+++ index.js
@@ -1,13 +1,184 @@
-let showDead = document.getElementById('dead')
 
-dead.onchange = function(e)
+function CheckboxToggleIndeterminate(element_action)
 {
-	if(dead.checked)
-	{
-		document.body.classList.add('show_dead')
+	if (element_action.readOnly) {
+		element_action.readOnly = false;
+		element_action.checked = true;
 	}
-	else
-	{
-		document.body.classList.remove("show_dead");
+	else if (element_action.checked) {
+		element_action.indeterminate = true;
+		element_action.readOnly = true;
 	}
 }
+
+function ToggleTagFiltersVisibility(element_action)
+{
+	const elementTagsContainer = document.getElementById("TagsContainer");
+	const hideClassName = "tag-filters-hide";
+	
+	const showFilters = elementTagsContainer.classList.contains(hideClassName);
+	
+	if (showFilters) {
+		elementTagsContainer.classList.remove(hideClassName);
+	}
+	else {
+		elementTagsContainer.classList.add(hideClassName);
+	}
+}
+
+function ToggleDeadWebsites(element_action)
+{
+	const elementTagsContainer = document.getElementById("TagsContainer");
+	const elementTagDead = elementTagsContainer.querySelectorAll("input[name='dead']")[0];
+	
+	if (element_action.checked) {
+		elementTagDead.readOnly = true;
+		elementTagDead.checked = false;
+		elementTagDead.indeterminate = true;
+	}
+	else {
+		elementTagDead.readOnly = false;
+		elementTagDead.checked = false;
+		elementTagDead.indeterminate = false;
+	}
+	
+	ToggleTag();
+}
+
+function ResetAllTags(element_action)
+{
+	const elementTagsContainer = document.getElementById("TagsContainer");
+	const elementsTags = elementTagsContainer.querySelectorAll("input[type='checkbox']");
+	
+	for (const elementTag of elementsTags) {
+		elementTag.checked = false;
+		elementTag.readOnly = true;
+		elementTag.indeterminate = true;
+	}
+	
+	const elementSimpleFilterDead = document.getElementById("SimpleFilterDead");
+	elementSimpleFilterDead.checked = true;
+	
+	ToggleTag();
+}
+
+function ToggleTag()
+{
+	const elementTagsContainer = document.getElementById("TagsContainer");
+	const elementSitesContainer = document.getElementById("SitesContainer");
+	const elementsTags = elementTagsContainer.querySelectorAll("input[type='checkbox']");
+	
+	const tagsOn = new Set();
+	const tagsOff = new Set();
+	
+	for (const elementTag of elementsTags) {
+		if (elementTag.indeterminate) {
+			continue;
+		}
+		let tagName = elementTag.getAttribute("name");
+		if (elementTag.checked) {
+			tagsOn.add(tagName);
+		}
+		else {
+			tagsOff.add(tagName);
+		}
+	}
+	
+	let previousHeadingTag = undefined;
+	let previousHeadingTagHasVisibleInside = false;
+	let previousHeadingSubtag = undefined;
+	let previousHeadingSubtagHasVisibleInside = false;
+	
+	const ProcessHeader = (elementSiteContainer) => {
+		const isSubtag = (elementSiteContainer?.id.indexOf(":") ?? -1) >= 0;
+		
+		const hideClassName = "hide-site";
+		if (previousHeadingSubtag !== undefined) {
+			if (!previousHeadingSubtagHasVisibleInside) {
+				if (!previousHeadingSubtag.classList.contains(hideClassName)) {
+					previousHeadingSubtag.classList.add(hideClassName);
+				}
+			}
+			else {
+				if (previousHeadingSubtag.classList.contains(hideClassName)) {
+					previousHeadingSubtag.classList.remove(hideClassName);
+				}
+			}
+		}
+		
+		if (isSubtag) {
+			previousHeadingSubtag = elementSiteContainer;
+			previousHeadingSubtagHasVisibleInside = false;
+		}
+		else {
+			if (previousHeadingTag !== undefined) {
+				if (!previousHeadingTagHasVisibleInside) {
+					if (!previousHeadingTag.classList.contains(hideClassName)) {
+						previousHeadingTag.classList.add(hideClassName);
+					}
+				}
+				else {
+					if (previousHeadingTag.classList.contains(hideClassName)) {
+						previousHeadingTag.classList.remove(hideClassName);
+					}
+				}
+			}
+			
+			previousHeadingTag = elementSiteContainer;
+			previousHeadingSubtag = undefined;
+			previousHeadingTagHasVisibleInside = false;
+			previousHeadingSubtagHasVisibleInside = false;
+		}
+	};
+	
+	for (const elementSiteContainer of elementSitesContainer.children) {
+		let siteTags = elementSiteContainer.getAttribute("data-tags")?.split(",") ?? [];
+		if (siteTags.length === 1 && siteTags[0].length === 0) {
+			siteTags.length = 0;
+		}
+		if (siteTags.length === 0) {
+			if (elementSiteContainer.id.length > 0) {
+				ProcessHeader(elementSiteContainer);
+			}
+			continue;
+		}
+		siteTags = new Set(siteTags);
+		
+		let siteIsVisible = (
+			(tagsOn.size === 0 || Array.from(tagsOn).every((tagName) => siteTags.has(tagName)))
+			&& (tagsOff.size === 0 || Array.from(tagsOff).every((tagName) => !siteTags.has(tagName)))
+		);
+		const hideClassName = "hide-site";
+		if (siteIsVisible) {
+			previousHeadingTagHasVisibleInside = true;
+			if (previousHeadingSubtag !== undefined) {
+				previousHeadingSubtagHasVisibleInside = true;
+			}
+			if (elementSiteContainer.classList.contains(hideClassName)) {
+				elementSiteContainer.classList.remove(hideClassName);
+			}
+		}
+		else {
+			if (!elementSiteContainer.classList.contains(hideClassName)) {
+				elementSiteContainer.classList.add(hideClassName);
+			}
+		}
+	}
+	
+	ProcessHeader();
+}
+
+(function ()
+{
+	for (const element_action of document.querySelectorAll("input[type='checkbox'][indeterminate]")) {
+		element_action.indeterminate = true;
+		element_action.readOnly = true;
+	}
+	
+	// Trigger onload event in all input tags that define the event.
+	for (const element_action of document.querySelectorAll("input[onload]")) {
+		element_action.dispatchEvent(new Event("load"));
+	}
+	
+	ToggleTag();
+})();
